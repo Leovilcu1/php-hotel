d6b0f09bdaa703d49ecdfac8675244264139f09e
LeonidVilcu <vilcu.leo@mail.ru>
2023-02-21 01:24:42 +0100

    php-hotel

--- index.php
+++ index.php
@@ -41,8 +41,9 @@
     ];
 
 
-foreach($hotels as  $hotel){
+foreach($hotels as $numb => $hotel){
         echo "<hr>";
+        echo $numb ;
         foreach($hotel as $key => $dati){
                 echo "$key : $dati";
                 echo "<br>";
@@ -50,3 +51,42 @@ foreach($hotels as  $hotel){
 }
 
 ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP-Hotel</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css">
+</head>
+<body>
+    <table class="table">
+        <thead>
+        
+        
+            <tr>
+            <th scope="col">#</th>
+            <?php foreach($hotel as $key => $dati){?>
+                <th scope="col"><?php echo $key ?></th>
+                <?php } ?>
+                
+            </tr>
+        </thead>
+        <tbody>
+        <?php foreach($hotels as $numb => $hotel){?>
+
+                <tr>
+                        <th scope="row"><?php echo $numb ?></th>
+                        <td><?php echo $hotel["name"] ?></td>
+                        <td><?php echo $hotel["description"] ?></td>
+                        <td><?php echo $hotel["parking"] ? "yes" : "now" ?></td>
+                        <td><?php echo $hotel["vote"] ?></td>
+                        <td><?php echo $hotel["distance_to_center"] ?> km</td>
+                </tr>
+        <?php } ?>
+        
+  </tbody>
+    </table>
+</body>
+</html>
